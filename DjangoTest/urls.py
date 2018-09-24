b5c00c858e00009b05cc83c4bd3075242cd0022a
sadmonad <sadmonad@gmail.com>
2018-09-24 18:35:23 +0300

    Added short names for url

--- DjangoTest/urls.py
+++ DjangoTest/urls.py
@@ -3,12 +3,13 @@ from django.conf.urls import url
 from django.contrib import admin
 from django.contrib.auth import views as auth_views
 from django_registration import views as register_views
-from DjangoTest.views import Blogger, Profile
+from DjangoTest.views import Blogger, Profile, Video
 
 urlpatterns = [
     path('accounts/login/', auth_views.LoginView.as_view(template_name='accounts/login.html'), name='login'),
     path('accounts/profile/', Profile.as_view()),
     path('accounts/register/', register_views.RegistrationView.as_view(template_name='accounts/login.html'), name='register'),
-    path('bloggers/', Blogger.as_view()),
+    path('bloggers/', Blogger.as_view(), name='bloggers'),
+    path('videos/', Video.as_view(), name='videos'),
     path('admin/', admin.site.urls),
 ]
